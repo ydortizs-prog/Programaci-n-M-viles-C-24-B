e9d0b92740b7aa6a281cc6814622c85c036facaf
Yony Denis Ortiz Sifuentes <yd.ortizs@gmail.com>
2026-08-26 00:31:04 -0500

    Proyecto inicial con menú del carrito

---
+++ semana02/lab02carritokotlin/Carrito.kt
@@ -0,0 +1,8 @@
+package com.ortiz.lab02carritokotlin
+
+
+fun main() {
+    println("=========================================")
+    println("   CARRITO DE COMPRAS - TIENDA TECSUP")
+    println("=========================================")
+}
