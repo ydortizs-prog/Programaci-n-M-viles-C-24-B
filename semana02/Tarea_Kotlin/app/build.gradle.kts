fa662795daa83961c9de9ae16ccb8ec8bd13584c
yonyortiz-blip <tu-email@gmail.com>
2026-08-30 15:56:02 -0500

    Agregué pues el  metodo calcularEstado

--- semana02/Tarea_Kotlin/app/build.gradle.kts
+++ semana02/Tarea_Kotlin/app/build.gradle.kts
@@ -29,6 +29,7 @@ android {
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
         targetCompatibility = JavaVersion.VERSION_11
+        isCoreLibraryDesugaringEnabled = true
     }
     buildFeatures {
         compose = true
@@ -51,4 +52,5 @@ dependencies {
     androidTestImplementation(libs.androidx.junit)
     debugImplementation(libs.androidx.compose.ui.test.manifest)
     debugImplementation(libs.androidx.compose.ui.tooling)
+    coreLibraryDesugaring("com.android.tools:desugar_jdk_libs:2.0.4")
 }
--- semana02/Tarea_Kotlin/app/src/main/java/com/ortiz/tareakotlin/Biblioteca.kt
+++ semana02/Tarea_Kotlin/app/src/main/java/com/ortiz/tareakotlin/Biblioteca.kt
@@ -1,11 +1,22 @@
 package com.ortiz.tareakotlin
+
 import java.time.LocalDate
+import java.time.temporal.ChronoUnit
 
-
-class Prestamo(
+data class Prestamo(
     val titulo: String,
     val tipoUsuario: String,
     val fechaPrestamo: LocalDate,
     val fechaEntrega: LocalDate,
     val fechaDevolucion: LocalDate
-)
+) {
+
+    fun calcularEstado(): String {
+        val diasDiferencia = ChronoUnit.DAYS.between(fechaEntrega, fechaDevolucion)
+        return if (diasDiferencia <= 0) {
+            "Devuelto a tiempo"
+        } else {
+            "Devuelto con $diasDiferencia dia(s) de atraso"
+        }
+    }
+}
