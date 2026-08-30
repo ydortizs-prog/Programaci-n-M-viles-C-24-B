72640976a72fac2c6a67c3aa3b47745ca61b73eb
yonyortiz-blip <tu-email@gmail.com>
2026-08-30 13:46:25 -0500

    Agregué  clase Prestamo con sus respectivos  atributos

---
+++ semana02/Tarea_Kotlin/app/src/main/java/com/ortiz/tareakotlin/Biblioteca.kt
@@ -0,0 +1,11 @@
+package com.ortiz.tareakotlin
+import java.time.LocalDate
+
+
+class Prestamo(
+    val titulo: String,
+    val tipoUsuario: String,
+    val fechaPrestamo: LocalDate,
+    val fechaEntrega: LocalDate,
+    val fechaDevolucion: LocalDate
+)
