ea28ddfa7d014c98ca2225f642fbb096c0d83190
ydortizs-prog <yd.ortizs@gmail.com>
2026-09-03 05:55:29 -0500

    Corrigui el  build.gradle.kts: agrega bloque plugins y dependencies faltantes

--- semana03/Lab03RegistroProducto/app/build.gradle.kts
+++ semana03/Lab03RegistroProducto/app/build.gradle.kts
@@ -1,3 +1,8 @@
+plugins {
+    alias(libs.plugins.android.application)
+    alias(libs.plugins.kotlin.compose)
+}
+
 android {
     namespace = "com.ortiz.lab03registroproducto"
     compileSdk {
@@ -30,4 +35,22 @@ android {
     buildFeatures {
         compose = true
     }
+}
+
+dependencies {
+    implementation(libs.androidx.core.ktx)
+    implementation(libs.androidx.lifecycle.runtime.ktx)
+    implementation(libs.androidx.activity.compose)
+    implementation(platform(libs.androidx.compose.bom))
+    implementation(libs.androidx.compose.ui)
+    implementation(libs.androidx.compose.ui.graphics)
+    implementation(libs.androidx.compose.ui.tooling.preview)
+    implementation(libs.androidx.compose.material3)
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.androidx.junit)
+    androidTestImplementation(libs.androidx.espresso.core)
+    androidTestImplementation(platform(libs.androidx.compose.bom))
+    androidTestImplementation(libs.androidx.compose.ui.test.junit4)
+    debugImplementation(libs.androidx.compose.ui.tooling)
+    debugImplementation(libs.androidx.compose.ui.test.manifest)
 }
