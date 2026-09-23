86e14eb0273da9a4db84dd5db6a04ac30fad5c9b
ydortizs-prog <yd.ortizs@gmail.com>
2026-09-23 02:37:25 -0500

    define rutas de navegacion para Tecsup Fit

--- semana05/TecsupFit/app/src/main/java/com/ortiz/tecsupfit/navigation/Screen.kt
+++ semana05/TecsupFit/app/src/main/java/com/ortiz/tecsupfit/navigation/Screen.kt
@@ -1,4 +1,14 @@
 package com.ortiz.tecsupfit.navigation
 
-class Screen {
+sealed class Screen(val route: String) {
+    object Home : Screen("home")
+    object ClassDetail : Screen("class_detail/{classId}") {
+        fun createRoute(classId: String) = "class_detail/$classId"
+    }
+    object Confirmation : Screen("confirmation/{classId}") {
+        fun createRoute(classId: String) = "class_detail/$classId/confirm"
+    }
+    object Reservations : Screen("reservations")
+    object Routines : Screen("routines")
+    object Profile : Screen("profile")
 }
