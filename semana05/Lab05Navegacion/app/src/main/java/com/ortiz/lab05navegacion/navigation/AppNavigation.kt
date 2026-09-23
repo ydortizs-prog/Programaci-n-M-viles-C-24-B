a6f5bb4bd1cdf848544fa097dd86193bb5412e65
ydortizs-prog <yd.ortizs@gmail.com>
2026-09-22 22:38:18 -0500

    Agrega Screen.kt con sealed class de rutas (Home, List, Profile, Detail) y AppNavigation.kt con NavHost

---
+++ semana05/Lab05Navegacion/app/src/main/java/com/ortiz/lab05navegacion/navigation/AppNavigation.kt
@@ -0,0 +1,39 @@
+package com.ortiz.lab05navegacion.navigation
+
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavType
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import androidx.navigation.navArgument
+import com.ortiz.lab05navegacion.screens.DetailScreen
+import com.ortiz.lab05navegacion.screens.HomeScreen
+import com.ortiz.lab05navegacion.screens.ListScreen
+import com.ortiz.lab05navegacion.screens.ProfileScreen
+
+@Composable
+fun AppNavigation() {
+    val navController = rememberNavController()
+
+    NavHost(
+        navController = navController,
+        startDestination = Screen.Home.route
+    ) {
+        composable(Screen.Home.route) {
+            HomeScreen(navController = navController)
+        }
+        composable(Screen.List.route) {
+            ListScreen(navController = navController)
+        }
+        composable(Screen.Profile.route) {
+            ProfileScreen(navController = navController)
+        }
+        composable(
+            route = Screen.Detail.route,
+            arguments = listOf(navArgument("itemId") { type = NavType.IntType })
+        ) { backStackEntry ->
+            val itemId = backStackEntry.arguments?.getInt("itemId") ?: 0
+            DetailScreen(itemId = itemId, navController = navController)
+        }
+    }
+}
---
+++ semana05/Lab05Navegacion/app/src/main/java/com/ortiz/lab05navegacion/navigation/Screen.kt
@@ -0,0 +1,10 @@
+package com.ortiz.lab05navegacion.navigation
+
+sealed class Screen(val route: String) {
+    object Home : Screen("home")
+    object List : Screen("list")
+    object Profile : Screen("profile")
+    object Detail : Screen("detail/{itemId}") {
+        fun createRoute(itemId: Int) = "detail/$itemId"
+    }
+}
